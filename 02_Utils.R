37c5a771aef0e8d164ade59a8f283799c5973bdc
AlejandraLLI <alelli3@hotmail.com>
2018-12-16 21:29:27 -0600

    Agregando cambios a preparacion datos

--- 02_Utils.R
+++ 02_Utils.R
@@ -3,3 +3,12 @@ getmode <- function(x) {
   uniqx <- unique(x)
   uniqx[which.max(tabulate(match(x, uniqx)))]
 }
+
+imputarLags<-function(x){
+  
+  non_na<-which(is.na(x)==FALSE)[1]
+  
+  if(non_na>1){
+  x[which(is.na(x))]<-x[non_na]
+  }
+}
